52fd359c2e528e44f65f9cc98e0e8d3f9748770a
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2025-10-14 21:21:53 +0200

    Añadidos mas dialogos
    Se han añadido dialogos para la escena1 y escena 2

--- Scripts/dialogue.gd
+++ Scripts/dialogue.gd
@@ -4,6 +4,6 @@ extends Node2D
 
 func _ready():
 	# Carga tu recurso de diálogo
-	var dialogue_res = load("res://DialogueView/Dialogues/prueba.dialogue")
+	var dialogue_res = load("res://DialogueView/Dialogues/scene1.dialogue")
 	# Inicia el diálogo en el nodo Balloon
 	balloon.start(dialogue_res, "amador_intro")

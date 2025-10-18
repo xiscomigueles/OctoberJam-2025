2a3d151c99f74b559cf1d8e7a87ccc9e33461384
xiscomigueles <miguelesdominguez@gmail.com>
2025-10-18 20:47:19 +0200

    Dialogos en los objetos

--- Scripts/Enviroment/interact_safe_box.gd
+++ Scripts/Enviroment/interact_safe_box.gd
@@ -1,6 +1,8 @@
 extends Interactable
 
 @export var dialogue: DialogueResource
+@export var node_no_key: String
+@export var node_key: String
 @onready var numpad: Numpad = $"../NumpadUI"
 
 
@@ -9,9 +11,17 @@ func _ready() -> void:
 
 
 func interact() -> void:
-	DialogueManager.show_dialogue_balloon(dialogue)
+	_show_dialogue()
 	await DialogueManager.dialogue_ended
-	numpad.show()
-	await numpad.ended
+	if not Global.has_basement_key:
+		numpad.show()
+		await numpad.ended
 	interaction_ended.emit()
 	print("Has key? " + str(Global.has_basement_key))
+
+
+func _show_dialogue() -> void:
+	var node: String = node_no_key
+	if Global.has_basement_key:
+		node = node_key
+	DialogueManager.show_dialogue_balloon(dialogue, node)

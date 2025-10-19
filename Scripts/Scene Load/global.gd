21ae4309fb81860eefa7841edf1b453e73888f11
Diego Ruiz Gil <diegoruiz037@gmail.com>
2025-10-19 17:53:44 +0200

    feat: basement door

--- Scripts/Scene Load/global.gd
+++ Scripts/Scene Load/global.gd
@@ -3,6 +3,7 @@ extends Node
 enum RoomType { SOTANO, KITCHEN }
 
 var has_basement_key: bool = false
+var basement_door_open: bool = false
 var paint_reveal: bool = false
 var has_homer_simpson: bool = false
 var door_kitchen_open: bool = false
